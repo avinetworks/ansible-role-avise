ca394bdcf3c6a9d8cec6829e9ae9c8beed5cd1a7
Chaitanya Deshpande <15983253+chaitanyaavi@users.noreply.github.com>
2019-02-26 11:34:36 +0530

    #AV-44232: Updated avise role (#31)
    * Updated avise role
    
    * Added tasks to get cloud id
    
    * #AV-44232 Updated se role to create se on controller

--- files/init/avise_watcher.sh
+++ files/init/avise_watcher.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+
+############################################################################
+#
+# AVI CONFIDENTIAL
+# __________________
+#
+# [2013] - [2018] Avi Networks Incorporated
+# All Rights Reserved.
+#
+# NOTICE: All information contained herein is, and remains the property
+# of Avi Networks Incorporated and its suppliers, if any. The intellectual
+# and technical concepts contained herein are proprietary to Avi Networks
+# Incorporated, and its suppliers and are covered by U.S. and Foreign
+# Patents, patents in process, and are protected by trade secret or
+# copyright law, and other laws. Dissemination of this information or
+# reproduction of this material is strictly forbidden unless prior written
+# permission is obtained from Avi Networks Incorporated.
+###
+
 while true
 do
   service avise start
